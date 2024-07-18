aea7d14e4f80d335107799e396c45a56983e4e2d
bravo1goingdark <kumarashutosh34169@gmail.com>
2024-07-18 17:09:43 +0530

    updated db

--- db/db.js
+++ db/db.js
@@ -4,10 +4,10 @@ import cors from 'cors';
 const app = express();
 const PORT = 3000;
 app.use(cors());
-const DB_URI = "mysql://avnadmin:PASS@pharma-backende-ak0704176-bdd5.i.aivencloud.com:16165/pharmacy_management_system";
+const DB_URI = "mysql://avnadmin:AVNS_Wq9z1CjMWf0xkkox-R4@mysql-1835da40-ak0704176-bdd5.c.aivencloud.com:16165/pharmacy_management_system"
 const connection = mysql.createConnection(DB_URI);
 
-connection.connect((err) => err ? console.error('Connection error:', err) : console.log("DB CONNECTED"));
+connection.connect(err => err ? console.error('Connection error:', err) : console.log("DB CONNECTED"));
 
 app.get('/api/medicine', (request ,response) => {
     const getMedicine = `SELECT Name,Category,Description,Price,PName,Quantity FROM medicine INNER JOIN pharmacy ON medicine.P_ID = pharmacy.PharmacyID`;
